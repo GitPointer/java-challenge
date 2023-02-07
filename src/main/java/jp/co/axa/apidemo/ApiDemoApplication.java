7c7b81cf0cd9a3e92e517a02c534efc0a7c02290
X@Pointer <4018280+GitPointer@users.noreply.github.com>
2023-02-08 01:35:13 +0900

    role based access to endpoints (#10)

--- src/main/java/jp/co/axa/apidemo/ApiDemoApplication.java
+++ src/main/java/jp/co/axa/apidemo/ApiDemoApplication.java
@@ -2,9 +2,10 @@ package jp.co.axa.apidemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
+@EnableWebMvc
 @SpringBootApplication
 public class ApiDemoApplication {
 
