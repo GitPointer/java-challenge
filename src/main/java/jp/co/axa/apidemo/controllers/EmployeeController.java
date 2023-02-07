b149cd973d62a57299a53b0231fb02a0697234da
X@Pointer <pro.vikas.tandon@gmail.com>
2023-02-08 01:33:24 +0900

    role based access to endpoints

--- src/main/java/jp/co/axa/apidemo/controllers/EmployeeController.java
+++ src/main/java/jp/co/axa/apidemo/controllers/EmployeeController.java
@@ -7,6 +7,7 @@ import jp.co.axa.apidemo.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -24,27 +25,32 @@ public class EmployeeController {
     }
 
     @GetMapping("/employees")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<List<EmployeeResponseDTO>> getEmployees() {
         return new ResponseEntity<>(employeeService.retrieveEmployees(), HttpStatus.OK);
 
     }
 
     @GetMapping("/employees/{employeeId}")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<EmployeeResponseDTO> getEmployee(@PathVariable(name="employeeId")Long employeeId) {
         return new ResponseEntity<>(employeeService.getEmployee(employeeId), HttpStatus.OK);
     }
 
     @PostMapping("/employees")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<EmployeeResponseDTO> saveEmployee(@RequestBody @Valid EmployeeRequestDTO employeeRequestDTO){
         return new ResponseEntity<>(employeeService.saveEmployee(employeeRequestDTO), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/employees/{employeeId}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deleteEmployee(@PathVariable(name="employeeId")Long employeeId){
         employeeService.deleteEmployee(employeeId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);    }
 
     @PutMapping("/employees/{employeeId}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<EmployeeResponseDTO> updateEmployee(@RequestBody @Valid EmployeeRequestDTO employeeRequestDTO,
                                @PathVariable(name="employeeId")Long employeeId){
         EmployeeResponseDTO emp = employeeService.getEmployee(employeeId);
