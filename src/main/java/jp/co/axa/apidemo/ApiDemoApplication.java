62e91e410070d821d15cba8f1f1d70223edcc303
X@Pointer <pro.vikas.tandon@gmail.com>
2023-02-08 22:58:32 +0900

    Logging addded

--- src/main/java/jp/co/axa/apidemo/ApiDemoApplication.java
+++ src/main/java/jp/co/axa/apidemo/ApiDemoApplication.java
@@ -1,5 +1,8 @@
 package jp.co.axa.apidemo;
 
+import jp.co.axa.apidemo.controllers.EmployeeController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -10,9 +13,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableCaching
 public class ApiDemoApplication {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ApiDemoApplication.class);
 
 	public static void main(String[] args) {
+		LOGGER.info("Starting Application...");
 		SpringApplication.run(ApiDemoApplication.class, args);
+		LOGGER.info("Application started successfully!");
 	}
 
 }
